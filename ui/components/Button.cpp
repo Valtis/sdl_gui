55d9462c1a1de03d8362e46bc3a6737473ba23c7
Valtis <erkka.kaaria@helsinki.fi>
2014-07-08 13:30:49 +0300

    Button class skeleton

---
+++ ui/components/Button.cpp
@@ -0,0 +1,23 @@
+/*
+ * Button.cpp
+ *
+ *  Created on: 8.7.2014
+ *      Author: valtis
+ */
+
+#include "Button.h"
+
+namespace sdl_gui {
+namespace component {
+
+Button::Button() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Button::~Button() {
+	// TODO Auto-generated destructor stub
+}
+}
+
+} /* namespace SDL_GUI */
---
+++ ui/components/Button.h
@@ -0,0 +1,17 @@
+
+#ifndef BUTTON_H_
+#define BUTTON_H_
+
+namespace sdl_gui {
+namespace component {
+
+class Button {
+public:
+	Button();
+	virtual ~Button();
+};
+
+} /* namespace component */
+} /* namespace SDL_GUI */
+
+#endif /* BUTTON_H_ */
