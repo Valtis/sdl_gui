09d0100b46396ddc0f30169fb083b6d359cdc653
Valtis <erkka.kaaria@helsinki.fi>
2014-07-14 16:36:19 +0300

    Test folder, modified travil to use that folder

---
+++ tests/WindowBaseTest.cpp
@@ -0,0 +1,14 @@
+#include "WindowBaseTest.h"
+
+namespace sdl_gui {
+
+WindowBaseTest::WindowBaseTest() {
+	// TODO Auto-generated constructor stub
+
+}
+
+WindowBaseTest::~WindowBaseTest() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace sdl_gui */
---
+++ tests/WindowBaseTest.h
@@ -0,0 +1,14 @@
+#ifndef WINDOWBASETEST_H_
+#define WINDOWBASETEST_H_
+
+namespace sdl_gui {
+
+class WindowBaseTest {
+public:
+	WindowBaseTest();
+	virtual ~WindowBaseTest();
+};
+
+} /* namespace sdl_gui */
+
+#endif /* WINDOWBASETEST_H_ */
