9ac602fd79dddbc4463b8892d7a2bed3308fca61
Valtis <erkka.kaaria@helsinki.fi>
2014-07-14 18:08:15 +0300

    Modified .travis.yml to install dependencies; bash script for pugixml download & build

---
+++ install-pugixml.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -ex
+# wget http://github.com/zeux/pugixml/releases/download/v1.4/pugixml-1.4.tar.gz
+tar -xzvf pugixml-1.4.tar.gz
+cd pugixml-1.4/scripts/
+cmake CMakeLists.txt 
+make all
