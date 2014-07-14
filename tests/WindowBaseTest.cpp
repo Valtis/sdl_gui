c41f6dbd70ee6c9310a6c175c418099b00b0dca3
Valtis <erkka.kaaria@helsinki.fi>
2014-07-14 17:30:17 +0300

    Added few mock tests for travis

--- tests/WindowBaseTest.cpp
+++ tests/WindowBaseTest.cpp
@@ -11,4 +11,12 @@ WindowBaseTest::~WindowBaseTest() {
 	// TODO Auto-generated destructor stub
 }
 
+void WindowBaseTest::always_pass() {
+	CPPUNIT_ASSERT(42 == 42);
+}
+
+void WindowBaseTest::always_fail() {
+	CPPUNIT_ASSERT(4 == 5);
+}
+
 } /* namespace sdl_gui */
--- tests/WindowBaseTest.h
+++ tests/WindowBaseTest.h
@@ -1,14 +1,37 @@
 #ifndef WINDOWBASETEST_H_
 #define WINDOWBASETEST_H_
 
+#include <cppunit/TestFixture.h>
+#include <cppunit/TestSuite.h>
+#include <cppunit/extensions/HelperMacros.h>
+
 namespace sdl_gui {
 
-class WindowBaseTest {
+class WindowBaseTest : public CppUnit::TestFixture {
+
+    CPPUNIT_TEST_SUITE(WindowBaseTest);
+    CPPUNIT_TEST(always_pass);
+    CPPUNIT_TEST(always_fail);
+    CPPUNIT_TEST_SUITE_END();
+
 public:
 	WindowBaseTest();
 	virtual ~WindowBaseTest();
+
+
+    void setUp(void) {}
+    void tearDown(void) {}
+
+private:
+	void always_pass();
+	void always_fail();
 };
 
+CPPUNIT_TEST_SUITE_REGISTRATION( WindowBaseTest );
 } /* namespace sdl_gui */
+
+
+
+
 
 #endif /* WINDOWBASETEST_H_ */
